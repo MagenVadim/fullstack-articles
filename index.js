0fd2284dd67701414f85afc8569dccabb49c1f22
Vadim Smagin <magenvadim@gmail.com>
2024-05-10 02:09:56 +0300

    #3 JWT has added

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send("0000000, Hello, my Friend")
 });
 
+
 app.post('/auth/login', (req, res)=>{
+    console.log(req.body)
+    const token = jwt.sign({
+        email: req.body.email,
+        fullname: "Eric Magnus"
+    }, 'sekretKey')
     res.json({
         success: true,
+        token 
     })
 })
 
