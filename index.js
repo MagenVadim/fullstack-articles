524469f9a676899ee7d2bdff1d4df882f3953427
Vadim Smagin <magenvadim@gmail.com>
2024-05-12 02:39:31 +0300

    #7 User creating has added

--- index.js
+++ index.js
@@ -3,28 +3,40 @@ import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import {validationResult} from 'express-validator';
 
+import bcrypt from 'bcrypt'
 import {registerValidattion} from './validations/auth.js';
+import UserModel from './models/User.js'
 
 mongoose
 .connect('mongodb+srv://magenvadim:X7LJbTLyB1gsr5BG@cluster0.bjxlrug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>console.log('DB is ok'))
 .catch((err)=>console.log('DB is error', err))
 
-
 const app = express()
 app.use(express.json())
 
-app.post('/auth/register', registerValidattion, (req, res)=>{
+
+app.post('/auth/register', registerValidattion, async (req, res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()){
         return res.status(400).json(errors.array())
     }
-    res.json({
-        success: true
-    })
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,        
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
+    });
+
+    const user = await doc.save();
+    res.json(user);
 })
-
-
 
 
 app.listen(4444, (err)=>{
