4aede91dd01d2f264afd31f4feb2311e3637bcff
Vadim Smagin <magenvadim@gmail.com>
2024-05-13 01:30:26 +0300

    #8 JWT has added to User_doc

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {registerValidattion} from './validations/auth.js';
 import UserModel from './models/User.js'
 
 mongoose
-.connect('mongodb+srv://magenvadim:X7LJbTLyB1gsr5BG@cluster0.bjxlrug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.connect('mongodb+srv://magenvadim:X7LJbTLyB1gsr5BG@cluster0.bjxlrug.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>console.log('DB is ok'))
 .catch((err)=>console.log('DB is error', err))
 
@@ -16,27 +16,56 @@ const app = express()
 app.use(express.json())
 
 
+
+
+
 app.post('/auth/register', registerValidattion, async (req, res)=>{
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json(errors.array())
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json(errors.array())
+        }
+    
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+    
+    
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,        
+            avatarUrl: req.body.avatarUrl,
+            passwordHash,
+        });
+    
+        const user = await doc.save();
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            }, 
+            'secret123',
+            {
+                expiresIn:'30d',
+            },
+        );
+
+        res.json({
+                ...user._doc,
+                token,
+            });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Failed to register a new user"
+        })
     }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,        
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    });
-
-    const user = await doc.save();
-    res.json(user);
 })
+
+
+
+
 
 
 app.listen(4444, (err)=>{
