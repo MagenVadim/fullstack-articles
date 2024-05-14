022bda6ea23681c49a5cfd5ec269bd88d2710d95
Vadim Smagin <magenvadim@gmail.com>
2024-05-14 14:50:30 +0300

    #12 code refactoring for UserController

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
-import {validationResult} from 'express-validator';
-
-import bcrypt from 'bcrypt'
 import {registerValidattion} from './validations/auth.js';
-import UserModel from './models/User.js'
 import checkAuth from './utils/checkAuth.js';
-
+import * as UserController from './controllers/UserController.js'
 
 mongoose
 .connect('mongodb+srv://magenvadim:X7LJbTLyB1gsr5BG@cluster0.bjxlrug.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
@@ -18,108 +13,11 @@ const app = express()
 app.use(express.json())
 
 
-app.post('/auth/login', async (req, res)=>{
-    try{
-        const user = await UserModel.findOne({email: req.body.email})
-        if(!user) {
-            res.status(404).json({
-                message: 'User is not found'
-            })
-        }
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-        if(!isValidPass){
-            return res.status(400).json({
-                message: 'Wrong login or password'
-            })
-        }
+app.post('/auth/login', UserController.login)
 
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            }, 
-            'secret123',
-            {
-                expiresIn:'30d',
-            },
-        );
+app.post('/auth/register', registerValidattion, UserController.register)
 
-        const {passwordHash, ...userData} = user._doc;
-        res.json({
-                ...userData,
-                token,
-            });
-
-    } catch(err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Failed to log in"
-        })
-    }
-})
-
-app.post('/auth/register', registerValidattion, async (req, res)=>{
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()){
-            return res.status(400).json(errors.array())
-        }
-    
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-    
-    
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,        
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hash,
-        });
-    
-        const user = await doc.save();
-
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            }, 
-            'secret123',
-            {
-                expiresIn:'30d',
-            },
-        );
-
-        const {passwordHash, ...userData} = user._doc;
-        res.json({
-                ...userData,
-                token,
-            });
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Failed to register a new user"
-        })
-    }
-})
-
-app.get('/auth/me', checkAuth , async (req, res)=>{    
-    try{
-        const user = await UserModel.findById(req.userId);
-        if(!user){
-            return res.status(404).json({
-                message: 'User is not found'
-            })
-        }
-        const {passwordHash, ...userData} = user._doc;
-        res.json({userData});
-
-    } catch(err) {
-        console.log(err);
-        res.status(500).json({
-            message: "Forbidden"
-        })
-    }
-});
+app.get('/auth/me', checkAuth , UserController.getMe);
 
 
 app.listen(4444, (err)=>{
