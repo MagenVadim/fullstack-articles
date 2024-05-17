6c6dc7796cbd1519e369ce0052f0fa4a026af928
Vadim Smagin <magenvadim@gmail.com>
2024-05-17 13:12:41 +0300

    #14 Image upload function has implemented

--- index.js
+++ index.js
@@ -4,19 +4,42 @@ import {registerValidattion, loginValidattion, postCreateValidattion} from './va
 import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controllers/UserController.js'
 import * as PostController from './controllers/PostController.js'
+import multer from 'multer'
 
 mongoose
 .connect('mongodb+srv://magenvadim:X7LJbTLyB1gsr5BG@cluster0.bjxlrug.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>console.log('DB is ok'))
 .catch((err)=>console.log('DB is error', err))
 
-const app = express()
-app.use(express.json())
+const app = express();
 
+
+const storage = multer.diskStorage({
+    destination:(_, __, cb)=>{
+        cb(null, 'uploads');
+    },
+    filename:(_, file, cb)=>{
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({storage: storage}).single('image');
+
+app.use(express.json())
 
 app.post('/auth/login', loginValidattion, UserController.login)
 app.post('/auth/register', registerValidattion, UserController.register)
 app.get('/auth/me', checkAuth , UserController.getMe);
+
+
+app.post('/upload', checkAuth, upload, (res, req)=>{
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    })
+})
+
+
+
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidattion, PostController.create);
